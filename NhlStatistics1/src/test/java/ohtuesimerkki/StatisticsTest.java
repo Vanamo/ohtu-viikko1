270d346bd6d309266547e51ef7d0d558c0399fd0
Vanamo Piirainen <vanamo.piirainen@helsinki.fi>
2018-10-30 17:21:11 +0200

    NHL-testeistä 2 feilaa

---
+++ NhlStatistics1/src/test/java/ohtuesimerkki/StatisticsTest.java
@@ -0,0 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ohtuesimerkki;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author vseppane
+ */
+public class StatisticsTest {
+ 
+    Reader readerStub = new Reader() {
+ 
+        public List<Player> getPlayers() {
+            ArrayList<Player> players = new ArrayList<Player>();
+ 
+            players.add(new Player("Semenko", "EDM", 4, 12));
+            players.add(new Player("Lemieux", "PIT", 45, 54)); //99
+            players.add(new Player("Kurri",   "EDM", 37, 53)); //90
+            players.add(new Player("Yzerman", "DET", 42, 56)); //98
+            players.add(new Player("Gretzky", "EDM", 35, 89)); //124
+ 
+            return players;
+        }
+    };
+ 
+    Statistics stats;
+
+    @Before
+    public void setUp(){
+        // luodaan Statistics-olio joka käyttää "stubia"
+        stats = new Statistics(readerStub);
+    } 
+    
+    /**
+     * Test of search method, of class Statistics.
+     */
+    @Test
+    public void searchFindsPlayer() {
+        String name = "Kurri";
+        Player expResult = new Player("Kurri",   "EDM", 37, 53);
+        Player result = stats.search(name);
+        assertEquals(name, result.getName());
+    }
+
+    @Test
+    public void playerDoesntExist() {
+        String name = "Vanamo";
+        assertEquals(null, stats.search(name));
+    }
+    
+    /**
+     * Test of team method, of class Statistics.
+     */
+    @Test
+    public void teamFindsPlayers() {
+        String teamName = "EDM";
+        List<String> expResult = new ArrayList<>();
+        expResult.add("Semenko");
+        expResult.add("Kurri");
+        expResult.add("Gretzky");
+        
+        List<Player> players = stats.team(teamName);
+        ArrayList<String> result = new ArrayList<>();
+        for (Player player : players) {
+            result.add(player.getName());
+        }
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void teamReturnsEmptyListIfNotFound() {
+        String teamName = "JEE";
+        List<Player> expResult = new ArrayList<>();
+        
+        List<Player> result = stats.team(teamName);
+        assertEquals(expResult, result);
+    }
+    
+    /**
+     * Test of topScorers method, of class Statistics.
+     */
+    @Test
+    public void returnsTop3Scorers() {
+        int howMany = 3;
+        Object[] expResult = {"Gretzky", "Lemieux", "Yzerman"};
+
+        List<Player> players = stats.topScorers(howMany);
+        Object[] result = new Object[3];
+        for (int i = 0; i <= howMany; i++) {
+            result[i] = players.get(i).getName();
+        }
+        
+        assertArrayEquals(expResult, result);
+    }
+    
+    @Test
+    public void returnsAllScorers() {
+        int howMany = 10;
+        List<String> expResult = new ArrayList<>();
+        expResult.add("Gretzky");
+        expResult.add("Lemieux");
+        expResult.add("Yzerman");
+        expResult.add("Kurri");
+        expResult.add("Semenko");
+
+        List<Player> players = stats.topScorers(howMany);
+        ArrayList<String> result = new ArrayList<>();
+        for (Player player : players) {
+            result.add(player.getName());
+        }
+        assertEquals(expResult, result);
+    }    
+    
+    @Test
+    public void returnsNoScorers() {
+        int howMany = -1;
+        List<Player> expResult = new ArrayList<>();
+
+        List<Player> result = stats.topScorers(howMany);
+        assertEquals(expResult, result);
+    }  
+}
