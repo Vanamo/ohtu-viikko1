0bc3b0ccff622b9665f130fddbddcec28e31027a
Vanamo Piirainen <vanamo.piirainen@helsinki.fi>
2018-12-03 15:34:03 +0200

    viikko 6 teht. 1

---
+++ Laskin/src/main/java/laskin/Nollaa.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laskin;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author vseppane
+ */
+public class Nollaa implements Komento {
+
+    private TextField tuloskentta;
+    private TextField syotekentta;
+    private Button nollaa;
+    private Button undo;
+    private Sovelluslogiikka sovellus;
+
+    public Nollaa(TextField tuloskentta, TextField syotekentta, Button nollaa, Button undo, Sovelluslogiikka sovellus) {
+        this.tuloskentta = tuloskentta;
+        this.syotekentta = syotekentta;
+        this.nollaa = nollaa;
+        this.undo = undo;
+        this.sovellus = sovellus;
+    }
+
+    @Override
+    public void suorita() {
+
+        sovellus.nollaa();
+
+        int laskunTulos = sovellus.tulos();
+
+        syotekentta.setText("");
+        tuloskentta.setText("" + laskunTulos);
+
+        if (laskunTulos == 0) {
+            nollaa.disableProperty().set(true);
+        } else {
+            nollaa.disableProperty().set(false);
+        }
+    }
+
+    @Override
+    public void peru() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
