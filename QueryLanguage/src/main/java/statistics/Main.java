fc09c2055f830a414567281c9c156bed300b4869
Vanamo Piirainen <vanamo.piirainen@helsinki.fi>
2018-12-03 17:05:12 +0200

    viikko 6 teht. 4

--- QueryLanguage/src/main/java/statistics/Main.java
+++ QueryLanguage/src/main/java/statistics/Main.java
@@ -6,7 +6,11 @@ public class Main {
 
     public static void main(String[] args) {
         Statistics stats = new Statistics(new PlayerReaderImpl("http://nhlstats-2013-14.herokuapp.com/players.txt"));
-
+        QueryBuilder query = new QueryBuilder();
+        Matcher m = query.playsIn("NYR")
+                         .hasAtLeast(10, "goals")
+                         .hasFewerThan(25, "goals")
+                         .build();
 //        Matcher m = new And( new HasAtLeast(10, "goals"),
 //                             new HasAtLeast(10, "assists"),
 //                             new PlaysIn("PHI")
@@ -16,7 +20,7 @@ public class Main {
 //                new HasAtLeast(60, "assists"),
 //                new HasAtLeast(85, "points")
 //        );
-        Matcher m = new HasFewerThan(1, "goals");
+//        Matcher m = new HasFewerThan(1, "goals");
 
         for (Player player : stats.matches(m)) {
             System.out.println(player);
---
+++ QueryLanguage/src/main/java/statistics/QueryBuilder.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package statistics;
+
+import statistics.matcher.*;
+
+/**
+ *
+ * @author vseppane
+ */
+class QueryBuilder {
+    Matcher matcher;
+    Matcher[] matchers;
+
+    public QueryBuilder() {
+        matcher = new And(new All());
+    }
+    
+    public Matcher build() {
+        return matcher;
+    }
+ 
+    QueryBuilder playsIn(String team) {
+        this.matcher = new And(matcher, new PlaysIn(team));
+        return this;
+    }
+    
+    QueryBuilder hasAtLeast(int value, String category) {
+        this.matcher = new And(matcher, new HasAtLeast(value, category));
+        return this;
+    }
+        
+    QueryBuilder hasFewerThan(int value, String category) {
+        this.matcher = new And(matcher, new HasFewerThan(value, category));
+        return this;
+    }
+}
